85a4b197ad44b7d2f97d0d5e7e35a5095ab9a0a5
calum <calumbousfield@gmail.com>
2026-08-28 17:17:01 +0100

    Stage 4: full CRUD

--- index.js
+++ index.js
@@ -63,7 +63,65 @@ app.post("/tasks", (req, res) => {
 
     tasks.push(newTask);
     res.status(201).json(newTask);
+
 });
+
+app.put("/tasks/:id", (req,res) => {
+    const id = Number(req.params.id);
+    const task = tasks.find((task) => task.id === id);
+
+    if (!task) {
+        return res.status(404).json({
+            error: `Task ${id} not found`
+        });
+    }
+
+     const { title, done } = req.body;
+
+    if (title === undefined && done === undefined) {
+        return res.status(400).json({
+            error: "Request must include title and/or done"
+        });
+    }
+
+    if (title !== undefined) {
+        if (typeof title !== "string" || title.trim() === "") {
+            return res.status(400).json({
+                error: "Title cannot be empty"
+            });
+        }
+
+        task.title = title.trim();
+    }
+
+    if (done !== undefined) {
+        if (typeof done !== "boolean") {
+            return res.status(400).json({
+                error: "Done must be true or false"
+            });
+        }
+
+        task.done = done;
+    }
+
+    res.status(200).json(task);
+});
+
+app.delete("/tasks/:id", (req, res) => {
+    const id = Number(req.params.id);
+    const taskIndex = tasks.findIndex((task) => task.id === id);
+
+    if (taskIndex === -1) {
+        return res.status(404).json({
+            error: `Task ${id} not found`
+        });
+    }
+
+    tasks.splice(taskIndex, 1);
+    res.status(204).send();
+});
+
+
 
 app.listen(port, ()=>{
     console.log("Server runnin at http://localhost:${port}");
